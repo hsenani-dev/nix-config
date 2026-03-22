a39ebd750e10fdf57287ceb303a8fe6dd88b00e9
Henry Senanian <henry@sinabi.dev>
2026-03-22 14:51:34 -0700

    clean up

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -20,23 +20,10 @@ in
       ...
     }:
     {
-
-      # test = builtins.listToAttrs (
-      #   lib.map (params: {
-      #     name = params.machine.name;
-      #     value = lib.nixosSystem { };
-      #   }) config.hosts
-      # );
-
       options = {
         nixosConfigurations = lib.mkOption {
           type = lib.types.attrs;
         };
-        # availableSystems = lib.mkOption {
-        #   type = lib.types.listOf lib.types.str;
-        #   default = systems;
-        #   description = "The list of systems defined in the flake.";
-        # };
       };
 
       config = {
