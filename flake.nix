e2b9ae7e8032e8c6c896d2b480211a9b81dd6e30
Henry Senanian <henry@sinabi.dev>
2026-03-22 01:17:03 -0700

    flake parts works

--- flake.nix
+++ flake.nix
@@ -1,19 +1,20 @@
 {
   description = "Henry's NixOS, nix-darwin and Home Manager Configuration";
   inputs = {
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs-unstable";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    systems.url = "github:nix-systems/default";
   };
   outputs =
     inputs:
 
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      debug = true;
-      systems = import inputs.systems;
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
 
       imports = [
         ./modules/nixos/default.nix
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,32 +1,17 @@
 { withSystem, inputs, ... }:
 {
-  perSystem =
-    { system, ... }:
-    {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [ ];
-        config = {
-          allowUnfree = true;
-        };
-      };
-
-      # Now use this configured pkgs in your packages, devShells, etc.
-      packages.my-package = pkgs.hello;
-    };
-
-  flake.nixosConfigurations.nix-os = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.my-machine = inputs.nixpkgs.lib.nixosSystem {
     modules = [
-      ./configuration.nix
-      inputs.nixpkgs.nixosModules.readOnlyPkgs
       (
-        { config, ... }:
+        { pkgs, ... }:
         {
-          # Use the configured pkgs from perSystem
-          nixpkgs.pkgs = withSystem config.nixpkgs.hostPlatform.system (
-            { pkgs, ... }: # perSystem module arguments
-            pkgs
-          );
+          imports = [
+            ./configuration.nix
+            inputs.home-manager.nixosModules.home-manager
+          ];
+
+          nixpkgs.config.allowUnfree = true;
+          nixpkgs.overlays = [ ];
         }
       )
     ];
