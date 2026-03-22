cc165b41c4d8df88a8ee375c68af4675a56b612a
Henry Senanian <henry@sinabi.dev>
2026-03-22 16:00:21 -0700

    so much to configure

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,6 +3,19 @@
   imports = [
     inputs.home-manager.nixosModules.home-manager
     inputs.nixos-cli.nixosModules.nixos-cli
+
+    # hardware
     ./machine
+
+    # desktop environment
+    ./de
+
+    # nix config
+    ./nix.nix
+
+    # cli
+    ./cli.nix
+    ./locale.nix
+    ./networking.nix
   ];
 }
---
+++ modules/nixos/input.nix
@@ -0,0 +1,8 @@
+{
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+  services.libinput.enable = true;
+  services.libinput.mouse.accelProfile = "flat";
+}
