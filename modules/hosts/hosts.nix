bf1bfe59254d1a0bed793bd0788d0a22403cb8ca
Henry Senanian <henry@sinabi.dev>
2026-03-22 21:56:50 -0700

    added filesystem

--- modules/hosts/hosts.nix
+++ modules/hosts/hosts.nix
@@ -5,11 +5,27 @@
       machine.name = "nixos-home";
       de.type = "cosmic";
 
-      modules = [ ];
-    }
-    {
-      machine.name = "mac-mini";
-      machine.system = "aarch64-darwin";
+      modules = [
+        {
+          fileSystems."/" = {
+            device = "/dev/disk/by-uuid/f3d40eaf-fa0a-4daa-83f2-6ba0fa0461f3";
+            fsType = "ext4";
+          };
+
+          fileSystems."/boot" = {
+            device = "/dev/disk/by-uuid/005A-48FD";
+            fsType = "vfat";
+            options = [
+              "fmask=0077"
+              "dmask=0077"
+            ];
+          };
+
+          swapDevices = [
+            { device = "/dev/disk/by-uuid/bff6d01b-be0d-4c85-835f-f11d6b631441"; }
+          ];
+        }
+      ];
     }
   ];
 }
