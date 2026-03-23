aa1aba251fdc4a87ce059503f904625ccc966307
Henry Senanian <henry@sinabi.dev>
2026-03-23 00:25:29 -0700

    set fonts

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,4 +1,9 @@
-{ params, config, ... }:
+{
+  params,
+  config,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ./pkgs.nix
@@ -11,7 +16,53 @@
     home = {
       username = params.user.name;
       homeDirectory = "/home/${params.user.name}";
+      packages = with pkgs; [
+        nerd-fonts.fira-code
+        font-awesome
+        noto-fonts-color-emoji
+        noto-fonts-monochrome-emoji
+        symbola
+        work-sans
+        corefonts
+        fira-go
+        fira-sans
+        lato
+        liberation_ttf
+        nerd-fonts.space-mono
+        nerd-fonts.symbols-only
+        source-serif
+        ubuntu-classic
+        unscii
+        cascadia-code
+      ];
     };
+
+    fonts = {
+      fontconfig = {
+        enable = true;
+        defaultFonts = {
+          serif = [
+            "Source Serif"
+            "Noto Color Emoji"
+          ];
+          sansSerif = [
+            "Work Sans"
+            "Noto Color Emoji"
+          ];
+          monospace = [
+            "FiraCode Nerd Font Mono"
+            "Font Awesome 6 Free"
+            "Font Awesome 6 Brands"
+            "Symbola"
+            "Noto Emoji"
+          ];
+          emoji = [
+            "Noto Color Emoji"
+          ];
+        };
+      };
+    };
+
     # Enable the Catppuccin theme
     catppuccin = {
       accent = "blue";
