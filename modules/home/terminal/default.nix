0a6ab3807cae47bc9900787fc2a5b9666eba076d
Henry Senanian <henry@sinabi.dev>
2026-03-31 22:18:43 -0700

    added nix shell functions

--- modules/home/terminal/default.nix
+++ modules/home/terminal/default.nix
@@ -18,6 +18,93 @@ let
     wormhole = "${pkgs.wormhole-rs}/bin/wormhole-rs";
     weather = "${lib.getExe pkgs.girouette} --quiet";
   };
+  fishShellFunctions = {
+    ns = {
+      body = ''
+        set -l cmd "nix flake show '.?submodules=1#'"
+                  echo "> $cmd"
+                  eval "$cmd"'';
+      description = "nix flake show shortcut - displays packages in a given flake.";
+    };
+    nfu.body = ''
+      set -l cmd "nix flake update $argv"
+      eval "$cmd"
+    '';
+    nb = {
+      body = ''
+        set -l derivation $argv[1]
+        set -l args $argv[2..-1]
+        set -l cmd "nix build --print-build-logs $args '.?submodules=1#$derivation'"
+        echo "> $cmd"
+        eval "$cmd"
+      '';
+      description = "nix build - usage nb <package name> <build args>";
+    };
+    nd = {
+      body = ''
+        set -l cmd "nix develop --print-build-logs '.?submodules=1#$argv'"
+        echo "> $cmd"
+        eval "$cmd"
+      '';
+      description = "nix build - usage nd <package name>";
+    };
+    nr = {
+      body = ''
+        set -l derivation $argv[1]
+        set -l args $argv[2..-1]
+        set -l cmd "nix run --print-build-logs '.?submodules=1#$derivation'"
+        if string length --quiet "$args"
+          set cmd "nix run --print-build-logs '.?submodules=1#$derivation' -- $args"
+        end
+        echo "> $cmd"
+        eval "$cmd"
+      '';
+      description = "nix run - usage: nr <package-name> <run args>";
+    };
+  };
+  shellExtra = ''
+    # nix flake show shortcut - displays packages in a given flake.
+    ns() {
+      local cmd="nix flake show '.?submodules=1#'"
+      echo "> $cmd"
+      eval "$cmd"
+    }
+
+    nfu() {
+      local cmd="nix flake update $@"
+      eval "$cmd"
+    }
+
+    # nix build - usage nb <package name>
+    nb() {
+      arg_array=($@)
+      derivation=''${arg_array[1]}
+      args=''${arg_array[@]:1}
+      local cmd="nix build --print-build-logs ''${args} '.?submodules=1#''${derivation}'"
+      echo "> $cmd"
+      eval "$cmd"
+    }
+
+    nd() {
+      local cmd="nix develop --print-build-logs '.?submodules=1#$@'"
+      echo "> $cmd"
+      eval "$cmd"
+    }
+
+    # nix run - usage: nr <package-name> <args>
+    nr() {
+      arg_array=($@)
+      derivation=''${arg_array[1]}
+      args=''${arg_array[@]:1}
+      if [[ -n ''${args} ]]; then
+        local cmd="nix run --print-build-logs '.?submodules=1#''${derivation}' -- ''${args}"
+      else
+        local cmd="nix run --print-build-logs '.?submodules=1#''${derivation}'"
+      fi
+      echo "> $cmd"
+      eval "$cmd"
+    }
+  '';
 in
 {
   imports = [
@@ -63,8 +150,17 @@ in
   };
 
   programs = {
-    bash.shellAliases = shellAliases;
-    fish.shellAliases = shellAliases;
-    zsh.shellAliases = shellAliases;
+    bash = {
+      shellAliases = shellAliases;
+      initExtra = shellExtra;
+    };
+    fish = {
+      shellAliases = shellAliases;
+      functions = fishShellFunctions;
+    };
+    zsh = {
+      shellAliases = shellAliases;
+      initExtra = shellExtra;
+    };
   };
 }
