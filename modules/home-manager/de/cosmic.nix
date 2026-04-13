5b84dee1ee3153d4cab41c76b695fc1edaa42d5a
Henry Senanian <henry@sinabi.dev>
2026-04-13 09:16:58 -0700

    sysinfo not in 25.11

--- modules/home-manager/de/cosmic.nix
+++ modules/home-manager/de/cosmic.nix
@@ -9,7 +9,6 @@
         cosmic-ext-applet-caffeine
         cosmic-ext-applet-external-monitor-brightness
         cosmic-ext-applet-minimon
-        cosmic-ext-applet-sysinfo
         cosmic-ext-calculator
         cosmic-ext-ctl
         cosmic-ext-tweaks
