a40e629c225542c26247615ef0cce40f6b9182ad
Henry Senanian <hsenanian@anduril.com>
2026-08-10 12:09:13 -0700

    remove grammarly

--- modules/home-manager/desktop/apps/chromium.nix
+++ modules/home-manager/desktop/apps/chromium.nix
@@ -5,7 +5,6 @@
       extensions = [
         { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # ProtonPass
         { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
-        { id = "kbfnbcaeplbcioakkpcpgfkobkghlhen"; } # Grammarly
         { id = "mdjildafknihdffpkfmmpnpoiajfjnjd"; } # Consent-O-Matic
         { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock for YouTube
         { id = "gebbhagfogifgggkldgodflihgfeippi"; } # Return YouTube Dislike
