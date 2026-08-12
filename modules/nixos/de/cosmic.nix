7eed72c6265840c7024094ce32af50f4caed8a2d
Henry Senanian <hsenanian@anduril.com>
2026-08-12 16:20:32 -0700

    fix dark mode in libreoffice

--- modules/nixos/de/cosmic.nix
+++ modules/nixos/de/cosmic.nix
@@ -1,7 +1,11 @@
 {
-  flake.modules.nixos.cosmic = {
+  flake.modules.nixos.cosmic = { pkgs, ... }: {
     services.desktopManager.cosmic.enable = true;
     services.displayManager.cosmic-greeter.enable = true;
     services.system76-scheduler.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      gnome-themes-extra
+    ];
   };
 }
