9858a3efabc3ca331bdced864cfb35cd71d07b0c
Henry Senanian <hsenanian@anduril.com>
2026-08-20 10:41:41 -0700

    simplify

--- modules/home-manager/desktop/apps/chromium.nix
+++ modules/home-manager/desktop/apps/chromium.nix
@@ -1,15 +1,12 @@
 {
   flake.modules.homeManager.base = {
-    programs.chromium = {
-      enable = true;
-      extensions = [
-        { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # ProtonPass
-        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
-        { id = "mdjildafknihdffpkfmmpnpoiajfjnjd"; } # Consent-O-Matic
-        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock for YouTube
-        { id = "gebbhagfogifgggkldgodflihgfeippi"; } # Return YouTube Dislike
-        { id = "glnpjglilkicbckjpbgcfkogebgllemb"; } # Okta
-      ];
-    };
+    programs.chromium.extensions = [
+      { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # ProtonPass
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
+      { id = "mdjildafknihdffpkfmmpnpoiajfjnjd"; } # Consent-O-Matic
+      { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock for YouTube
+      { id = "gebbhagfogifgggkldgodflihgfeippi"; } # Return YouTube Dislike
+      { id = "glnpjglilkicbckjpbgcfkogebgllemb"; } # Okta
+    ];
   };
 }
